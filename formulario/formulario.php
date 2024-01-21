54864db02c86c4443ee1380df5d6a66047475cc7
Raphael Pitol <raphaelpitoljuliani@gmail.com>
2024-01-21 11:12:01 -0300

    Criando validações com msg em cada input

--- formulario/formulario.php
+++ formulario/formulario.php
@@ -5,26 +5,28 @@
 
 <?php
 if (count($_POST) > 0) {
+
+     $erros = [];
      // isset($_POST['nome'])
      //filtra os inputs POST com nome se não existe
      if (!filter_input(INPUT_POST, 'nome')) {
-          echo "Nome Obrigatório <br>";
+          $erros['nome'] = "Nome Obrigatório";
      }
      if (filter_input(INPUT_POST, 'nascimento')) {
           $data = DateTime::createFromFormat('d/m/Y', $_POST['nascimento']);
           if (!$data) {
-               echo "Data deve estar no padrão dd/mm/aaaa <br>";
+               $erros['nascimento'] = "Data deve estar no padrão dd/mm/aaaa";
           }
      }
 
      //filtra a variavel $_POST['email'] se é um valido
      if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-          echo "Email inválido <br>";
+          $erros['email'] = "Email inválido";
      }
 
      //filtra a variavel $_POST['site'] se é um valido
      if (!filter_var($_POST['site'], FILTER_VALIDATE_URL)) {
-          echo "Site inválido <br>";
+          $erros['site'] = "Site inválido ";
      }
 
      $filhosConfig = [
@@ -35,47 +37,73 @@ if (count($_POST) > 0) {
           FILTER_VALIDATE_INT,
           $filhosConfig
      ) && $_POST['filhos'] != 0) {
-          echo 'Quantidade de filhos inválida <br>';
+          $erros['filhos'] = 'Quantidade de filhos inválida';
      }
 
-     $salarioConfig = ['options' => ['decimal'=> ',']];
-     if(!filter_var($_POST['salario'],
-     FILTER_VALIDATE_FLOAT, $salarioConfig)){
-          echo 'Salário inválido <br>';
+     $salarioConfig = ['options' => ['decimal' => ',']];
+     if (!filter_var(
+          $_POST['salario'],
+          FILTER_VALIDATE_FLOAT,
+          $salarioConfig
+     )) {
+          $erros['salario'] = 'Salário inválido';
      }
 }
 ?>
+<!-- <?php foreach ($erros as $erro) : ?>
+     <div class="alert alert-warning" role="alert"><?= "" //$erro 
+                                                       ?></div>
 
-<form action="#" method="post">
+<?php endforeach ?> -->
+<form action="#" method="post" class="needs-validation" novalidate>
      <div class="form-row">
-          <div class="form-group col-md-9">
+          <div class="form-group col-md-9 ">
                <label for="nome">Nome</label>
-               <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome" value="<?= $_POST['nome'] ?>">
+               <input type="text" class="form-control <?= $erros['nome'] ? 'is-invalid' : ''?>" id="nome" name="nome" placeholder="Nome" value="<?= $_POST['nome'] ?>">
+               <div class="invalid-tooltip">
+                    <?= $erros['nome'] ?>
+               </div>
           </div>
           <div class="form-group col-md-3">
                <label for="nascimento">Nascimetno</label>
-               <input type="text" class="form-control" id="nascimento" name="nascimento" placeholder="Nascimetno" value="<?= $_POST['nascimento'] ?>">
+               <input type="text" class="form-control <?= $erros['nascimento'] ? 'is-invalid' : ''?>" id="nascimento" name="nascimento" placeholder="Nascimetno" value="<?= $_POST['nascimento'] ?>">
+               <div class="invalid-tooltip">
+                    <?= $erros['nascimento'] ?>
+               </div>
           </div>
      </div>
      <div class="form-row">
           <div class="form-group col-md-6">
                <label for="email">E-mail</label>
-               <input type="text" class="form-control" id="email" name="email" placeholder="E-mail" value="<?= $_POST['email'] ?>">
+               <input type="text" class="form-control <?= $erros['email'] ? 'is-invalid' : ''?>" id="email" name="email" placeholder="E-mail" value="<?= $_POST['email'] ?>">
+               <div class="invalid-tooltip">
+                    <?= $erros['email'] ?>
+               </div>
           </div>
           <div class="form-group col-md-6">
                <label for="site">Site</label>
-               <input type="text" class="form-control" id="site" name="site" placeholder="Site" value="<?= $_POST['site'] ?>">
+               <input type="text" class="form-control <?= $erros['site'] ? 'is-invalid' : ''?>" id="site" name="site" placeholder="Site" value="<?= $_POST['site'] ?>">
+               <div class="invalid-tooltip">
+                    <?= $erros['site'] ?>
+               </div>
           </div>
      </div>
      <div class="form-row">
           <div class="form-group col-md-6">
                <label for="filhos">QTD Filhos</label>
-               <input type="number" class="form-control" id="filhos" name="filhos" placeholder="QTD Filhos" min="0" max="10" value="<?= $_POST['filhos'] ?>">
+               <input type="number" class="form-control <?= $erros['filhos'] ? 'is-invalid' : ''?>" id="filhos" name="filhos" placeholder="QTD Filhos" min="0" max="10" value="<?= $_POST['filhos'] ?>">
+               <div class="invalid-tooltip">
+                    <?= $erros['filhos'] ?>
+               </div>
           </div>
           <div class="form-group col-md-6">
                <label for="salario">Salario</label>
-               <input type="text" class="form-control" id="salario" name="salario" placeholder="Salario" value="<?= $_POST['salario'] ?>">
+               <input type="text" class="form-control <?= $erros['salario'] ? 'is-invalid' : ''?>" id="salario" name="salario" placeholder="Salario" value="<?= $_POST['salario'] ?>">
+               <div class="invalid-tooltip">
+                    <?= $erros['salario'] ?>
+               </div>
           </div>
      </div>
+
      <button class="btn btn-primary btn-lg">Enviar</button>
 </form>
